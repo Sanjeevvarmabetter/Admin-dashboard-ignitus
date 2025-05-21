c4b9249791b4d7ed55f2f299b5e917ad4405d7d5
Sanjeevvarmabetter <sanjeevvarmacode@gmail.com>
2025-05-21 19:12:27 +0530

    added

--- client/src/pages/unauthorized.js
+++
@@ -1,7 +0,0 @@
-export default function Unauthorized() {
-    return (
-      <div className="min-h-screen bg-black text-white flex items-center justify-center">
-        <h1 className="text-2xl">Access Denied</h1>
-      </div>
-    );
-  }
